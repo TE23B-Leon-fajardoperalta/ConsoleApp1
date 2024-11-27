5f4d300e09b0b9c96280a7b13f81f5361a425bdf
Fajardo peralta Leon <leon.fajardoperalta@elev.ga.ntig.se>
2024-11-27 15:06:00 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿List<string> valja = ["", "dog1", "dog2", "dog3", "dog4"];
+
+
+static int GetChoice(List<string> options)
+{
+    for (int i = 0; i < options.Count; i++)
+    {
+        Console.WriteLine(options[i]);
+    }
+
+    Console.WriteLine("välj en");
+
+    int result = 0;
+
+    while (result <= 0 || result >= options.Count+1)
+    {
+
+        string read = Console.ReadLine();
+
+        int.TryParse(read, out result);
+
+    }
+
+    return result;
+}
+
+
+int wow = GetChoice(["dog1", "dog2"]);
+
+
+// int wow = getchoice(["cat1", "cat2"]);
+
+Console.WriteLine(valja[wow]);
+Console.ReadLine();
